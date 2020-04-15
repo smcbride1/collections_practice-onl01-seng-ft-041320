1999133bf6eb8122b4d2aaccc90b0bde0fb77474
Stephen McBride <stevemcbride3@gmail.com>
2020-04-15 22:09:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,4 +13,20 @@ def sort_array_char_count(strings)
     a.length <=> b.length
   end
 end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    a[1] <=> b[2]
+  end
+end
+
+def reverse_array(integers)
+  integers.sort.reverse
+end
+
+def kesha_maker(strings)
+  strings.each do |string|
+    modified_string_array = string.chars
+    modified_string_array[2] = "$"
+    final_string = modified_string_array.join("")
     
